b200a0952552b12b17d005236105b395aaba6437
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-04-25 22:59:15 -0300

    testing h and w icons

--- frontend/src/components/BenefitsAndvideo/BenefitsandVideo.jsx
+++ frontend/src/components/BenefitsAndvideo/BenefitsandVideo.jsx
@@ -3,37 +3,13 @@ import { FaLeaf } from "react-icons/fa";
 const BenefitsandVideo = () => {
   return (
     <div className="flex flex-wrap justify-center gap-4">
-      {/* <div className="w-full">
-        <h1 className="text-senter font-sans-700 text-3xl text-gray-700">
-          Making your skin look younger and healthier is now much easier!
-        </h1>
-      </div>
-      <div className="w-full ">
-        <p className="text-center font-sans-400 text-md text-gray-600 px-3">
-          Not all creams provide effective results, as some brands may offer
-          products with ingredients or formulas that have been proven not to
-          work, simply because they are cheaper. At If Beauty, our mission is to
-          provide the best cream possible, with ingredients that have been
-          proven to work over our 30 years of working with our patients.
-        </p>
-      </div>
-      <div className="w-full flex justify-center py-4">
-        <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          src="https://cdn.shopify.com/videos/c/o/v/7e8ec8551dd14572bc6f291c3a33378e.mp4"
-          className="w-[80%] lg:w-[30%] rounded-full"
-        />
-      </div> */}
       <div className="w-full flex flex-wrap justify-center">
         <div className="w-1/2 lg:w-1/4 flex flex-wrap justify-center px-2 lg:px-8">
-          <div className="w-auto h-[70px] flex justify-center px-4">
+          <div className="w-[70px] h-[70px] flex justify-center ">
             <img
               src="https://cdn.shopify.com/s/files/1/0436/0673/7049/files/brote.png?v=1712615097"
               alt=""
-              className="w-[100%]"
+              className="w-[100%] h-full"
             />
           </div>
           <div className="py-6">
